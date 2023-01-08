db2194c882b43e43e682a09b3cb8682f54a7dec1
vslongo <vslongo@gmail.com>
2023-01-08 14:01:35 -0300

    #

--- JavaScript IV - POO/aula03-exercicio18-CriandoClasses/User.js
+++ JavaScript IV - POO/aula03-exercicio18-CriandoClasses/User.js
@@ -1,20 +1,20 @@
 class User {
-    constructor(fullname, email, password){
+    constructor(fullname, email, password) {
         this.fullname = fullname
         this.email = email
         this.password = password
 
     }
 
-    login(email, password){
-        if(this.email === email && this.password === password ){
+    login(email, password) {
+        if (this.email === email && this.password === password) {
             console.log("O seu login foi bem sucedido")
-        }else{
+        } else {
             console.log("Seu email ou senha estão incorretos")
         }
     }
 }
 
-const user1 =  new User("Vinicius Slongo", "teste@gmail.com", "teste123")
+const user1 = new User("Vinicius Slongo", "teste@gmail.com", "teste123")
 user1.login("teste@gmail.com", "teste123")
 console.log(user1)
