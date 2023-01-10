f80d4e947f89224e7d6712823ef4b2cfb15cec29
vslongo <vslongo@gmail.com>
2023-01-10 16:44:59 -0300

    fix: indentation and errors

--- JavaScript IV - POO/aula08-exercicio20-classe-DOM/Input.js
+++ JavaScript IV - POO/aula08-exercicio20-classe-DOM/Input.js
@@ -1,7 +1,9 @@
-import { Component } from "./Component.js";
+import {
+    Component
+} from "./Component.js";
 
 export class Input extends Component {
-  constructor(parent, options) {
-    super('input', parent, options)
-  }
+    constructor(parent, options) {
+        super('input', parent, options)
+    }
 }
