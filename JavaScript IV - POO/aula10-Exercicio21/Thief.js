f7adcbe4616a9f2335c94e9e8644737a3b22debb
vslongo <vslongo@gmail.com>
2023-01-18 09:55:06 -0300

    Thief

--- JavaScript IV - POO/aula10-Exercicio21/Thief.js
+++ JavaScript IV - POO/aula10-Exercicio21/Thief.js
@@ -1,5 +1,4 @@
 const Character = require("./Character");
-
 module.exports = class Thief extends Character {
   attack(targetCharacter) {
     targetCharacter.lifepoints -= (this.attackPts - targetCharacter.defensePts) * 2
